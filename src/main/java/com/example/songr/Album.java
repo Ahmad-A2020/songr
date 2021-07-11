45990c2a6db1d0f9144e3676b4f2350c191bed00
Ahmad-A92-3 <ahmad.alsyad92@gmail.com>
2021-07-11 22:25:54 +0300

    complete the lab

---
+++ src/main/java/com/example/songr/Album.java
@@ -0,0 +1,57 @@
+package com.example.songr;
+
+public class Album {
+    public String title;
+    private String artist;
+    private String imageUrl;
+    private int songCount;
+    private int length;
+
+    public Album(String title, String artist, String imageUrl, int songCount, int length) {
+        this.title = title;
+        this.artist = artist;
+        this.imageUrl = imageUrl;
+        this.songCount = songCount;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.songr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.*;
+import org.junit.Assert;
+
 
 @SpringBootTest
 class SongrApplicationTests {
+	Album al = new Album("song","NB","https://i.ytimg.com/vi/OzxMjBEazas/maxresdefault.jpg",5,300);
 
 	@Test
-	void contextLoads() {
+	void getterTitle() {
+		assertEquals("song",al.getTitle());
 	}
-
 }
